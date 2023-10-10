3665de84a596b67d3e6cd376a836efad10d8d18b
Ishan Mahesh <68223115+IshanMahesh@users.noreply.github.com>
2023-10-11 01:10:43 +0530

    implemented pid_control.cpp

--- pid/pid_control.cpp
+++ pid/pid_control.cpp
@@ -9,6 +9,27 @@
  * @return double
  */
 double PID::PIDCONTROLLER::compute(double target_setpoint,
-                                   double actual_velocity) {
-  return 0.0;
+                                   double actual_velocity, ) {
+  
+
+// Calculating the error
+    double error = target_velocity - actual_velocity;
+
+    // The value for the Proportional term
+    double Pout = kp * error;
+
+    // The value for the Integral term
+    integral += error * dt;
+    double Iout = ki * integral;
+
+    // Finding Derivative term
+    double derivative = (error - pre_error) / dt;
+    double Dout = kd * derivative;
+
+    // Calculating total output
+    double output = Pout + Iout + Dout;
+
+    return output;
+
+  
 }
