f5ae5ba432c71ba43dc792b58fbf201462f31b85
Ishan Mahesh <68223115+IshanMahesh@users.noreply.github.com>
2023-10-10 22:18:37 -0400

    Add files via upload

--- pid/pid_control.cpp
+++ pid/pid_control.cpp
@@ -1,5 +1,8 @@
 #include "pid_control.hpp"
 
+#include <cmath>
+#include <iostream>
+
 /**
  * @brief The method is used to compute the new velocity value based on
  * target setpoint and actual velocity and considering the pid constants
@@ -9,32 +12,33 @@
  * @return double
  */
 double PID::PIDCONTROLLER::compute(double target_setpoint,
-                                   double actual_velocity)
-   double previous_error = 0;
-   double current_error = 0;
-   double total_error = 0;
-   double time = 10;
-   double final_velocity;
-   for (int dt = 1; dt <= time; dt++) {
-      
-      // calculating the error
-      current_error = target_setpoint - actual_velocity;
+                                   double actual_velocity) {
+  double previous_error = 0;
+  double current_error = 0;
+  double total_error = 0;
+  double time = 10;
+  double final_velocity;
+  for (int dt = 1; dt <= time; dt++) {
+    // calculating the error
+    current_error = target_setpoint - actual_velocity;
 
-      // calculating the derivative error
-      double derivative_error = abs(current_error - previous_error);
+    // calculating the derivative error
+    double derivative_error = abs(current_error - previous_error);
 
-      // calculating the integral error
-      double integral_error = total_error * dt;
+    // calculating the integral error
+    double integral_error = total_error * dt;
 
-      // Updating previous error
-      previous_error = current_error;
+    // Updating previous error
+    previous_error = current_error;
 
-      //updating total error
-      total_error += current_error;
+    // updating total error
+    total_error += current_error;
 
-      // calculating the total output 
-      double output = (kp * current_error) + (kd * derivative_error)+ (ki * integral_error);
+    // calculating the total output
+    double output =
+        (kp * current_error) + (kd * derivative_error) + (ki * integral_error);
 
-      final_velocity += output;
+    final_velocity += output;
   }
-  return final_velocity; 
+  return final_velocity;
+}
